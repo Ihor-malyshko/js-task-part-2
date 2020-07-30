84bdc8667df79f7ad0abd2e5cdd8f071ceff1119
Ihor-malyshko <malishkoio@gmail.com>
2020-07-30 10:15:14 +0300

    task 7 on

--- module-7/task-7.js
+++ module-7/task-7.js
@@ -1,141 +1,141 @@
-// console.log("task-7");
+console.log("task-7");
 
-///////////////////////////////////////////////////////
-// // task 1
-///////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// task 1
+/////////////////////////////////////////////////////
 
-// const categories = document.querySelectorAll("#categories .item").length;
+const categories = document.querySelectorAll("#categories .item").length;
 
-// console.log(`В списке ${categories} категории.`);
+console.log(`В списке ${categories} категории.`);
 
-// const listsCategoriesRef = document.querySelectorAll("#categories .item");
-// // console.dir(listsRef);
+const listsCategoriesRef = document.querySelectorAll("#categories .item");
+// console.dir(listsRef);
 
-// listsCategoriesRef.forEach((item) =>
-//   console.log(
-//     `Категория: ${item.querySelector("h2").textContent},
-//   Количество элементов: ${item.querySelectorAll("li").length}`
-//   )
-// );
+listsCategoriesRef.forEach((item) =>
+  console.log(
+    `Категория: ${item.querySelector("h2").textContent},
+  Количество элементов: ${item.querySelectorAll("li").length}`
+  )
+);
 
-///////////////////////////////////////////////////////
-// // task 2
-///////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// task 2
+/////////////////////////////////////////////////////
 
-// const ingredients = [
-//   "Картошка",
-//   "Грибы",
-//   "Чеснок",
-//   "Помидоры",
-//   "Зелень",
-//   "Приправы",
-// ];
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
+];
 
-// const ingredientsList = ingredients.map((ingredient) => {
-//   const li = document.createElement("li");
-//   li.textContent = ingredient;
-//   return li;
-// });
-// document.querySelector("#ingredients").append(...ingredientsList);
+const ingredientsList = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  return li;
+});
+document.querySelector("#ingredients").append(...ingredientsList);
 
-// /////////////////////////////////////////////////////
-// // task 3
-// /////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// task 3
+/////////////////////////////////////////////////////
 
-// const images = [
-//   {
-//     url: "https://picsum.photos/300/200",
-//     alt: "White and Black Long Fur Cat",
-//   },
-//   {
-//     url: "https://picsum.photos/300/202",
-//     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-//   },
-//   {
-//     url: "https://picsum.photos/300/201",
-//     alt: "Group of Horses Running",
-//   },
-// ];
+const images = [
+  {
+    url: "https://picsum.photos/300/200",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://picsum.photos/300/202",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://picsum.photos/300/201",
+    alt: "Group of Horses Running",
+  },
+];
 
-// const createItemGallery = (image) => {
-//   const item = document.createElement("li");
-//   item.classList.add("gallery__item");
+const createItemGallery = (image) => {
+  const item = document.createElement("li");
+  item.classList.add("gallery__item");
 
-//   const img = document.createElement("img");
-//   img.src = image.url;
-//   img.alt = image.alt;
-//   img.width = 500;
+  const img = document.createElement("img");
+  img.src = image.url;
+  img.alt = image.alt;
+  img.width = 500;
 
-//   item.appendChild(img);
-//   return item;
-// };
+  item.appendChild(img);
+  return item;
+};
 
-// const createGallery = images.map((image) => createItemGallery(image));
-// document.querySelector("#gallery").append(...createGallery);
+const createGallery = images.map((image) => createItemGallery(image));
+document.querySelector("#gallery").append(...createGallery);
 
-// /////////////////////////////////////////////////////
-// // task 4
-// /////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// task 4
+/////////////////////////////////////////////////////
 
-// const decrement = document.querySelector("[decrement]");
-// const increment = document.querySelector("[increment]");
+const decrement = document.querySelector("[decrement]");
+const increment = document.querySelector("[increment]");
 
-// const changeValue = (event) => {
-//   if (event.target.getAttribute("data-action") === "decrement") {
-//     // -1
-//     document.querySelector("#value").textContent -= 1;
-//   } else {
-//     // - (-1) = +1
-//     document.querySelector("#value").textContent -= -1;
-//     // document.querySelector("#value").textContent++;
-//   }
-// };
+const changeValue = (event) => {
+  if (event.target.getAttribute("data-action") === "decrement") {
+    // -1
+    document.querySelector("#value").textContent -= 1;
+  } else {
+    // - (-1) = +1
+    document.querySelector("#value").textContent -= -1;
+    // document.querySelector("#value").textContent++;
+  }
+};
 
-// decrement.addEventListener("click", changeValue);
-// increment.addEventListener("click", changeValue);
+decrement.addEventListener("click", changeValue);
+increment.addEventListener("click", changeValue);
 
-// /////////////////////////////////////////////////////
-// // task 5
-// /////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// task 5
+/////////////////////////////////////////////////////
 
-// const inputNameRef = document.querySelector("#name-input");
-// const spanNameRef = document.querySelector("#name-output");
+const inputNameRef = document.querySelector("#name-input");
+const spanNameRef = document.querySelector("#name-output");
 
-// inputNameRef.addEventListener("blur", (event) => {
-//   spanNameRef.textContent = event.target.value;
-// });
+inputNameRef.addEventListener("blur", (event) => {
+  spanNameRef.textContent = event.target.value;
+});
 
-// /////////////////////////////////////////////////////
-// // task 6
-// /////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// task 6
+/////////////////////////////////////////////////////
 
-// const inputValidRef = document.querySelector("#validation-input");
-// inputValidRef.addEventListener("blur", (event) => {
-//   // console.dir(event.target.attributes[2].value);
-//   // console.dir(event.target.dataset.length);
-//   // console.dir(inputValidRef.getAttribute("data-length"));
-//   if (event.target.value.length == inputValidRef.getAttribute("data-length")) {
-//     inputValidRef.classList.add("valid");
-//     inputValidRef.classList.remove("invalid");
-//   } else {
-//     inputValidRef.classList.add("invalid");
-//     inputValidRef.classList.remove("valid");
-//   }
-// });
+const inputValidRef = document.querySelector("#validation-input");
+inputValidRef.addEventListener("blur", (event) => {
+  // console.dir(event.target.attributes[2].value);
+  // console.dir(event.target.dataset.length);
+  // console.dir(inputValidRef.getAttribute("data-length"));
+  if (event.target.value.length == inputValidRef.getAttribute("data-length")) {
+    inputValidRef.classList.add("valid");
+    inputValidRef.classList.remove("invalid");
+  } else {
+    inputValidRef.classList.add("invalid");
+    inputValidRef.classList.remove("valid");
+  }
+});
 
-// /////////////////////////////////////////////////////
-// // task 7
-// /////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// task 7
+/////////////////////////////////////////////////////
 
-// const inputSizeRef = document.querySelector("#font-size-control");
-// const spanSizeRef = document.querySelector("#text-size");
-// inputSizeRef.addEventListener("mouseup", (event) => {
-//   spanSizeRef.style.fontSize = event.target.value + "px";
-// });
+const inputSizeRef = document.querySelector("#font-size-control");
+const spanSizeRef = document.querySelector("#text-size");
+inputSizeRef.addEventListener("mouseup", (event) => {
+  spanSizeRef.style.fontSize = event.target.value + "px";
+});
 
-// /////////////////////////////////////////////////////
-// // task 8
-// /////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// task 8
+/////////////////////////////////////////////////////
 
 const randomColor = () => {
   const colorR = Math.round(Math.random() * 255);
